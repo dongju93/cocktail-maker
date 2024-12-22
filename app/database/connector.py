0f6649efbed61cb3ef1ef142e6a16da7f6ed80ff
dongju93 <spdlqj011@gmail.com>
2024-12-23 02:15:35 +0900

    :card_file_box: Fix db name on context manager

--- app/database/connector.py
+++ app/database/connector.py
@@ -3,19 +3,24 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator  # noqa: UP035
 
 from dotenv import load_dotenv
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorCollection,
+    AsyncIOMotorDatabase,
+)
 
 load_dotenv(dotenv_path="app/.env")
 MONGODB_URL: str | None = os.getenv("mongodb_url")
 
 
 @asynccontextmanager
-async def mongodb_conn() -> AsyncGenerator[AsyncIOMotorClient, None]:
+async def mongodb_conn(collection: str) -> AsyncGenerator[AsyncIOMotorCollection, None]:
     if MONGODB_URL is None:
         raise ValueError("MONGODB_URL is not set")
     uri: str = MONGODB_URL
     conn: AsyncIOMotorClient = AsyncIOMotorClient(uri, serverSelectionTimeoutMS=5000)
+    db: AsyncIOMotorDatabase = conn["cocktail-db"]
     try:
-        yield conn
+        yield db[collection]
     finally:
         conn.close()

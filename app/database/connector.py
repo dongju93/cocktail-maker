92f03df884db79ecac69f2e0cbbc082849e766fb
dongju93 <spdlqj011@gmail.com>
2025-01-06 02:33:39 +0900

    :adhesive_bandage: Use environ

--- app/database/connector.py
+++ app/database/connector.py
@@ -10,13 +10,11 @@ from motor.motor_asyncio import (
 )
 
 load_dotenv(dotenv_path="app/.env")
-MONGODB_URL: str | None = os.getenv("mongodb_url")
+MONGODB_URL: str = os.environ["mongodb_url"]
 
 
 @asynccontextmanager
 async def mongodb_conn(collection: str) -> AsyncGenerator[AsyncIOMotorCollection, None]:
-    if MONGODB_URL is None:
-        raise ValueError("MONGODB_URL is not set")
     uri: str = MONGODB_URL
     conn: AsyncIOMotorClient = AsyncIOMotorClient(uri, serverSelectionTimeoutMS=5000)
     db: AsyncIOMotorDatabase = conn["cocktail-db"]

d892361e195746708d7dbb2d22e3f17b591d97ff
dongju93 <spdlqj011@gmail.com>
2025-05-17 15:40:11 +0900

    :wrench: Add gunicorn config

---
+++ app/gunicorn.conf.py
@@ -0,0 +1,16 @@
+import multiprocessing
+
+bind = "0.0.0.0:8000"  # Use default port
+workers = multiprocessing.cpu_count() * 2 + 1  # Recommended
+worker_class = "uvicorn.workers.UvicornWorker"
+max_requests = 1000  # Restart workers for preventing memory leaks
+max_requests_jitter = (
+    50  # Individual workers will be restarted with a this random delay
+)
+accesslog = "-"  # stdout for access log
+errorlog = "-"
+timeout = 60  # Worker timeout in seconds
+preload_app = (
+    True  # Process the application code before the worker processes are forked
+)
+proc_name = "cocktail_maker"  # Process name

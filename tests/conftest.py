6e9bc5e5605cf2e79cc37ccff2e02cc6a0f23997
dongju93 <spdlqj011@gmail.com>
2025-05-18 13:33:42 +0900

    :wrench: Change dir to app folder then test it

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
-from main import app
+from os import chdir
+from pathlib import Path
 
-cocktail_maker = app
+# change directory to app directory
+chdir(Path(__file__).parent.parent / "app")
+
+from main import cocktail_maker  # noqa: E402 # type: ignore[import]
+
+api_service = cocktail_maker

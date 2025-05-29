6995d2fdce2010b8c30c4b0cc550107c515f05be
dongju93 <spdlqj011@gmail.com>
2025-05-29 19:17:19 +0900

    :wrench: Use new syntax that handle uvicorn workers

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -2,7 +2,7 @@ from multiprocessing import cpu_count
 
 bind = "0.0.0.0:8000"  # Use default port
 workers = cpu_count() * 2 + 1  # Recommended
-worker_class = "uvicorn.workers.UvicornWorker"
+worker_class = "uvicorn_worker.UvicornWorker"
 max_requests = 1000  # Restart workers for preventing memory leaks
 max_requests_jitter = (
     50  # Individual workers will be restarted with a this random delay

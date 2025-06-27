664403efe0d2a640f31c6aafd373071596c8c85a
dongju93 <spdlqj011@gmail.com>
2025-06-28 06:25:05 +0900

    :zap: Use vite-oxc for performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-oxc'
 
 
 // https://vite.dev/config/

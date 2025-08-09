c9caf9bc448a84fabe910be34a3bac0a1644f233
dongju93 <spdlqj011@gmail.com>
2025-08-09 21:31:43 +0900

    :recycle: Async mongodb connection favor of PyMongo API

--- app/database/connector.py
+++ app/database/connector.py
@@ -3,11 +3,9 @@ from os import environ
 from typing import AsyncGenerator, Generator  # noqa: UP035
 
 from dotenv import load_dotenv
-from motor.motor_asyncio import (
-    AsyncIOMotorClient,
-    AsyncIOMotorCollection,
-    AsyncIOMotorDatabase,
-)
+from pymongo import AsyncMongoClient
+from pymongo.asynchronous.collection import AsyncCollection
+from pymongo.asynchronous.database import AsyncDatabase
 from sqlmodel import Session
 
 from .table import engine
@@ -18,15 +16,15 @@ SQLITE_PATH: str = environ["SQLITE_PATH"]
 
 
 @asynccontextmanager
-async def mongodb_conn(collection: str) -> AsyncGenerator[AsyncIOMotorCollection]:
-    conn: AsyncIOMotorClient = AsyncIOMotorClient(
+async def mongodb_conn(collection: str) -> AsyncGenerator[AsyncCollection]:
+    conn: AsyncMongoClient = AsyncMongoClient(
         MONGODB_URL, serverSelectionTimeoutMS=5000
     )
-    db: AsyncIOMotorDatabase = conn["cocktail-db"]
+    db: AsyncDatabase = conn["cocktail-db"]
     try:
         yield db[collection]
     finally:
-        conn.close()
+        await conn.close()
 
 
 @contextmanager
